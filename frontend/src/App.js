404ce2b12a397966e49324477531b7b89b492cce
Harry Craven <harrycraven.dev@gmail.com>
2020-10-03 10:03:03 +0100

    Adding HomeScreen component into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import HomeScreen from "./Screens/HomeScreen";
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <Header />
         <main className="py-3">
           <Container>
-            <h1>ECommerce</h1>
+            <HomeScreen />
           </Container>
         </main>
         <Footer />
