b2270173f99a9b3eddad970df876ff2ca9aad048
Harry Craven <harrycraven.dev@gmail.com>
2020-10-16 10:03:43 +0100

    Adding Auth middleware.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      res.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorised, token failed.");
+    }
+  }
+
+  if (!req.headers.authorization) {
+    res.status(401);
+    throw new Error("Not authorised, no token.");
+  }
+});
