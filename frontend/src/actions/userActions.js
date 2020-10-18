f8d6c5f2ed09ea68e2605aec836b2209864dac7d
Harry Craven <harrycraven.dev@gmail.com>
2020-10-18 10:38:34 +0100

    Adding getUserDetails to actions.

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL} from '../constants/userConstants';
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL} from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -67,6 +67,38 @@ export const register = (name, email, password) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: USER_REGISTER_FAIL,
+            data:
+              error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+          });
+    }
+}
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_DETAILS_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState()
+    
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+    
+        const { data } = await axios.get(`/api/users/${id}`, config)
+    
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            data
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
             data:
               error.response && error.response.data.message
                 ? error.response.data.message
