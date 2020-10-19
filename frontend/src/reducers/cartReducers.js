10343923815cdc645bd4a1bf2c75048223e7e8fa
Harry Craven <harrycraven.dev@gmail.com>
2020-10-19 08:30:34 +0100

    Adding Save Shipping Address reducer.

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,6 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS, CART_SAVE_PAYMENT_METHOD } from "../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.data;
@@ -27,6 +27,16 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           (item) => item.product !== action.data
         ),
       };
+      case CART_SAVE_SHIPPING_ADDRESS:
+        return {
+          ...state,
+          shippingAddress: action.data
+        }
+      case CART_SAVE_PAYMENT_METHOD:
+        return {
+          ...state,
+          paymentMethod: action.data
+        }
     default:
       return state;
   }
