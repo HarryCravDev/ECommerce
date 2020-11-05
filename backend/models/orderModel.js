589266e5b20cf245f6d3bc595ea2b0b04de31fb0
Harry Craven <harrycraven.dev@gmail.com>
2020-11-05 16:27:54 +0000

    Error fix - OrderItems array.

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -14,34 +14,27 @@ const orderSchema = mongoose.Schema(
         image: { type: String, required: true },
         price: { type: Number, required: true },
         product: {
-          product: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
             ref: "Product",
-          },
         },
       },
     ],
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
-      postalCode: { type: String, required: true },
+      postcode: { type: String, required: true },
       country: { type: String, required: true },
     },
-    paymentMethod: {
-      type: Boolean,
-      required: true,
-    },
+    // paymentMethod: {
+    //   type: Boolean,
+    //   required: true,
+    // },
     paymentResult: {
       id: { type: String },
       status: { type: String },
       update_time: { type: String },
       email_adress: { type: String },
-    },
-    taxPrice: {
-      type: Number,
-      required: true,
-      default: 0.0,
     },
     shippingPrice: {
       type: Number,
