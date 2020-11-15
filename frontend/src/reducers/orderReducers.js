d5738aeb55601deb119bf65a012771c1fb800c3a
Harry Craven <harrycraven.dev@gmail.com>
2020-11-15 13:42:24 +0000

    Adding detailsOrderReducer to reducers.

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,10 +1,10 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from '../constants/orderConstants';
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from '../constants/orderConstants';
 
 export const createOrderReducer = (state = {}, action) => {
-    console.log('ORDER REDUCER: ', action.data);
     switch(action.type) {
         case ORDER_CREATE_REQUEST: 
         return {
+            ...state,
             loading: true
         }
         case ORDER_CREATE_SUCCESS: 
@@ -14,6 +14,30 @@ export const createOrderReducer = (state = {}, action) => {
             order: action.data
         }
         case ORDER_CREATE_FAIL: 
+        return {
+            loading: false,
+            error: action.data
+        }
+        default:
+            return {
+                state
+            }
+    }
+} 
+
+export const detailsOrderReducer = (state = { loading: true, orderItems: [], shippingAddress: {} }, action) => {
+    switch(action.type) {
+        case ORDER_DETAILS_REQUEST: 
+        return {
+            ...state,
+            loading: true
+        }
+        case ORDER_DETAILS_SUCCESS: 
+        return {
+            loading: false,
+            order: action.data
+        }
+        case ORDER_DETAILS_FAIL: 
         return {
             loading: false,
             error: action.data
