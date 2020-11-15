938f35f420cbcfbe124386acd302a896da44f5af
Harry Craven <harrycraven.dev@gmail.com>
2020-11-15 13:41:06 +0000

    Adding getOrderById to routes.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-    orderProducts
+    orderProducts,
+    getOrderById
 } = require("../controllers/orderController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(protect, orderProducts);
+router.route("/:id").get(protect, getOrderById);
 
 
 module.exports = router;
