c0dc828fc599f0b162c36b677b0245e97e189dd7
Harry Craven <harrycraven.dev@gmail.com>
2020-11-18 11:35:52 +0000

    Added Bootstrap Modal.

--- frontend/src/Screens/UserListScreen.js
+++ frontend/src/Screens/UserListScreen.js
@@ -1,23 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../Components/Message";
 import Loader from "../Components/Loader";
-import { listUsers } from "../actions/userActions";
+import { listUsers, deleteUser } from "../actions/userActions";
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
+
+  // Test Modal Code
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // On Click User ID
+  const [userId, setUserId] = useState("");
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
-  useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-  const deleteHandler = (id) => {
-    console.log(id);
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, userInfo, history, successDelete]);
+
+  const deleteHandler = () => {
+    console.log(`DELETE USER: ${userId}`);
+    dispatch(deleteUser(userId));
+    setUserId("");
+    handleClose();
+  };
+
+  // Test Modal Code
+  const modalHandler = (id) => {
+    setUserId(id);
+    handleShow();
   };
 
   return (
@@ -35,7 +62,7 @@ const UserListScreen = () => {
               <th>NAME</th>
               <th>EMAIL</th>
               <th>ADMIN</th>
-              <th></th>
+              <th>DELETE OR EDIT USER</th>
             </tr>
           </thead>
           <tbody>
@@ -54,19 +81,40 @@ const UserListScreen = () => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
-                    <Button
-                      variant="danger"
-                      className="btn-sm"
-                      onClick={() => deleteHandler(user._id)}
-                    >
-                      <i className="fas fa-trash"></i>
+                  <Button
+                    variant="danger"
+                    className="btn-sm"
+                    // onClick={handleShow}
+                    onClick={() => modalHandler(user._id)}
+                  >
+                    <i className="fas fa-trash"></i>
+                  </Button>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                    <Button variant="light" className="btn-sm">
+                      Edit User
                     </Button>
                   </LinkContainer>
                 </td>
               </tr>
             ))}
           </tbody>
+          {/* Test Modal Code */}
+          <>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Remove User</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Are you sure? Press Close to cancel.</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={deleteHandler}>
+                  Delete User
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </>
         </Table>
       )}
     </>
