b6fa7232e252fba9e70df7dcb9c87a0d002eb828
Harry Craven <harrycraven.dev@gmail.com>
2020-11-20 17:07:52 +0000

    Adding routes for deleteProduct, updateProduct, createProduct.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const {
   getProducts,
   getProductById,
+  deleteProduct,
+  updateProduct,
+  createProduct,
 } = require("../controllers/productController");
+const { protect, isAdmin } = require("../middleware/authMiddleware");
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/").get(getProducts).post(protect, isAdmin, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct);
 
 module.exports = router;
