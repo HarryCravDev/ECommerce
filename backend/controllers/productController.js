d46d60f83747fe28ea6dd3732ba873607ed1d833
Harry Craven <harrycraven.dev@gmail.com>
2020-11-20 17:07:29 +0000

    Adding deleteProduct, createProduct, updateProduct.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,7 +9,7 @@ exports.getProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
-//  @desc   Fetch single product by id
+//  @desc   Fetch a product by id
 //  @route  GET /api/products/:id
 //  @access Public
 exports.getProductById = asyncHandler(async (req, res) => {
@@ -20,5 +20,73 @@ exports.getProductById = asyncHandler(async (req, res) => {
   } else {
     res.status(404);
     throw new Error(`Product not found`);
+  }
+});
+
+//  @desc   Delete a product by id
+//  @route  DELETE /api/products/:id
+//  @access Private/Admin
+exports.deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed." });
+  } else {
+    res.status(404);
+    throw new Error(`Product not found`);
+  }
+});
+
+//  @desc   Create a product
+//  @route  POST  /api/products/:id
+//  @access Private/Admin
+exports.createProduct = asyncHandler(async (req, res) => {
+  const product = new Product.create({
+    name: "Same Name",
+    price: 50,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample Category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample Description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+//  @desc   Update a product
+//  @route  PUT  /api/products/:id
+//  @access Private/Admin
+exports.updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found.");
   }
 });
