e731c8902a54407dbec865510d1155d361b92f97
Harry Craven <harrycraven.dev@gmail.com>
2020-11-22 17:48:24 +0000

    Adding route for getAllOrders.

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,10 +5,14 @@ const {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getAllOrders,
 } = require("../controllers/orderController");
-const { protect } = require("../middleware/authMiddleware");
+const { protect, isAdmin } = require("../middleware/authMiddleware");
 
-router.route("/").post(protect, orderProducts);
+router
+  .route("/")
+  .post(protect, orderProducts)
+  .get(protect, isAdmin, getAllOrders);
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
